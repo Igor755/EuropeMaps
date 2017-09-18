92df4ea05d66a44ef5fa31e2ff7113d802d7c6ab
Sonikpalms <metlin.igor@gmail.com>
2017-09-18 23:26:44 +0300

    latlng

--- app/src/main/java/com/sonikpalms/europemaps/MainActivity.java
+++ app/src/main/java/com/sonikpalms/europemaps/MainActivity.java
@@ -1,16 +1,27 @@
 package com.sonikpalms.europemaps;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 import static com.sonikpalms.europemaps.R.id.map;
 
@@ -35,39 +46,42 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
 
 
-        // Set a preference for minimum and maximum zoom.
-        //mMap.setMinZoomPreference(6.0f);
-        // mMap.setMaxZoomPreference(14.0f);
-
-
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
-        //map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-        //private GoogleMap mMap;
-// Create a LatLngBounds that includes Australia.
+      
 
 
         //север восток, юго запад
         LatLngBounds Europe = new LatLngBounds(
                 new LatLng(50, 10), new LatLng(50, 10));
 
-// Set the camera to the greatest possible zoom level that includes the
-// bounds
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(Europe.getCenter(), 0));
         map.setLatLngBoundsForCameraTarget(Europe);
         map.setMinZoomPreference(3.6f);
         map.setMaxZoomPreference(3.6f);
-        //map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
 
-      // map.setOnMapClickListener(OnMapClickListener );
+        map.getUiSettings().setCompassEnabled(false);
+        map.getUiSettings().setMapToolbarEnabled(false);
+        map.getUiSettings().setRotateGesturesEnabled(false);
 
-        //final LatLng MELBOURNE = new LatLng(50, 20);
-//        Marker melbourne = map.addMarker(new MarkerOptions()
-        //.position(MELBOURNE)
-        //              .title("Melbourne")
-        //            .snippet("Population: 4,137,400"));
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+
+
+                Toast toast = Toast.makeText(getApplicationContext(),"onMapClick: " + latLng.latitude + "," + latLng.longitude,Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER,0,0);
+                toast.show();
+            }
+        });
+
+
+
+
 
 
     }
